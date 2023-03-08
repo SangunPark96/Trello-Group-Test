b695ec3ed2c7f8040d565b5a4c9604e2c1879906
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2023-03-08 13:32:40 -0500

    Create Nav.js

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+  function Nav() {    
+      return (
+        <nav>
+          <div>
+            <div>
+              <ul>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      );
+    }
