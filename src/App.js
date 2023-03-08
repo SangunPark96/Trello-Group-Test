529679be850654a452bc6638cfa5303a51842d75
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-08 11:35:43 -0500

    Creates Home and adjusts imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import Footer from "./components/common/Footer";
-import Home from "./components/home/Home";
+import Home from "./components/shows/Home/Home";
 import Nav from "./components/common/Nav";
 import Show from "./components/shows/Show";
 import ShowsEditForm from "./components/shows/ShowsEditForm";
