dd503c101dbc8416392753d345cbe9f6f7818ef6
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-08 13:36:55 -0500

    Adjust imports

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import Footer from "./components/shows/common/Footer";
-import Home from "./components/shows/Home/Home";
-import Nav from "./components/common/Nav";
+import Home from "./components/shows/home/Home";
+// import Nav from "./components/common/Nav";
 import Show from "./components/shows/Show";
 import ShowsEditForm from "./components/shows/ShowsEditForm";
-import ShowsIndex from "./components/shows/ShowsIndex";
+import ShowsIndex from "./components/shows/showsIndex";
 import ShowsNewForm from "./components/shows/ShowsNewForm";
-import Movie from "./components/movies/Movie";
-import MoviesIndex from "./components/movies/MoviesIndex";
-import MoviesEditForm from "./components/movies/MoviesEditForm";
-import MoviesNewForm from "./components/movies/MoviesNewForm";
+// import Movie from "./components/movies/Movie";
+// import MoviesIndex from "./components/movies/MoviesIndex";
+// import MoviesEditForm from "./components/movies/MoviesEditForm";
+// import MoviesNewForm from "./components/movies/MoviesNewForm";
 
 function App() {
   return (
     <div className="wrapper">
       <Router>
-        <Nav />
+        {/* <Nav /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shows" element={<ShowsIndex />} />
           <Route path="/shows/new" element={<ShowsNewForm />} />
           <Route path="/shows/:id" element={<Show />} />
           <Route path="/shows/:id/edit" element={<ShowsEditForm />} />
-          <Route path="/movies" element={<MoviesIndex />} />
+          {/* <Route path="/movies" element={<MoviesIndex />} />
           <Route path="/movies/new" element={<MoviesNewForm />} />
           <Route path="/movies/:id" element={<Movie />} />
-          <Route path="/movies/:id/edit" element={<MoviesEditForm />} />
+          <Route path="/movies/:id/edit" element={<MoviesEditForm />} /> */}
         </Routes>
         <Footer />
       </Router>
